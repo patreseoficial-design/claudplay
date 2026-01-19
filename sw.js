be22e91bdaf73deebfa44b896c4b87cb55d469df
patreseoficial-design <patreseoficial@gmail.com>
2026-01-19 14:23:33 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,23 @@
-self.addEventListener("install", (event) => {
-  self.skipWaiting();
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open("v1").then(function(cache) {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/login.html",
+        "/cadastro.html",
+        "/profile.html",
+        "/mario-banner.jpeg",
+        "/Racepromaxbanner.jpeg"
+      ]);
+    })
+  );
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
